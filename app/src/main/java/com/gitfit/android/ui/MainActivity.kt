81368b20762e9b9a03097ea03e67508463350c49
Adrian <adriantwarowski1@gmail.com>
2019-11-17 21:05:08 +0100

    Initial commit

---
+++ app/src/main/java/com/gitfit/android/ui/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.gitfit.android.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.gitfit.android.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
