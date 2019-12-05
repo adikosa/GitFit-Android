43196f88f4efb1e38d8e6f426e47c6081e86e53e
Adrian <adriantwarowski1@gmail.com>
2019-12-05 23:37:51 +0100

    profile layout

--- app/src/main/java/com/gitfit/android/ui/home/profile/ProfileFragment.kt
+++ app/src/main/java/com/gitfit/android/ui/home/profile/ProfileFragment.kt
@@ -4,9 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import com.gitfit.android.R
 
@@ -21,11 +19,6 @@ class ProfileFragment : Fragment() {
     ): View? {
         profileViewModel =
             ViewModelProviders.of(this).get(ProfileViewModel::class.java)
-        val root = inflater.inflate(R.layout.fragment_profile, container, false)
-        val textView: TextView = root.findViewById(R.id.text_notifications)
-        profileViewModel.text.observe(this, Observer {
-            textView.text = it
-        })
-        return root
+        return inflater.inflate(R.layout.fragment_profile, container, false)
     }
 }
--- app/src/main/java/com/gitfit/android/ui/home/profile/ProfileViewModel.kt
+++ app/src/main/java/com/gitfit/android/ui/home/profile/ProfileViewModel.kt
@@ -1,13 +1,5 @@
 package com.gitfit.android.ui.home.profile
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class ProfileViewModel : ViewModel() {
-
-    private val _text = MutableLiveData<String>().apply {
-        value = "This is profile Fragment"
-    }
-    val text: LiveData<String> = _text
-}
+class ProfileViewModel : ViewModel()
