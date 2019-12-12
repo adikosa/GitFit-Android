f6939146ee0c7b015bbed6801756620499fa6534
Adrian <adriantwarowski1@gmail.com>
2019-12-12 19:12:06 +0100

    journal layout (only one activity type - drinking coffee)

--- app/src/main/java/com/gitfit/android/ui/home/journal/JournalFragment.kt
+++ app/src/main/java/com/gitfit/android/ui/home/journal/JournalFragment.kt
@@ -6,9 +6,11 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.gitfit.android.R
+import kotlinx.android.synthetic.main.fragment_journal.*
 
 class JournalFragment : Fragment() {
 
@@ -21,11 +23,38 @@ class JournalFragment : Fragment() {
     ): View? {
         dashboardViewModel =
             ViewModelProviders.of(this).get(JournalViewModel::class.java)
-        val root = inflater.inflate(R.layout.fragment_journal, container, false)
-        val textView: TextView = root.findViewById(R.id.text_dashboard)
-        dashboardViewModel.text.observe(this, Observer {
-            textView.text = it
-        })
-        return root
+        return inflater.inflate(R.layout.fragment_journal, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        recycler_view.setHasFixedSize(true)
+        recycler_view.layoutManager = LinearLayoutManager(context)
+        recycler_view.adapter = MyAdapter((1..20).toList())
+    }
+
+    private inner class MyAdapter constructor(private val devices: List<Int>) :
+        RecyclerView.Adapter<MyAdapter.MyViewHolder>() {
+
+        private inner class MyViewHolder(item: View) : RecyclerView.ViewHolder(item) {
+            val textView: TextView = item.findViewById(R.id.date_time_text)
+
+        }
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+
+            val view = LayoutInflater.from(parent.context)
+                .inflate(R.layout.coffee_activity_card_view, parent, false)
+
+            return MyViewHolder(view)
+        }
+
+        override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+            holder.textView.text = devices[position].toString()
+        }
+
+        override fun getItemCount(): Int {
+            return devices.size
+        }
+
     }
 }
--- app/src/main/java/com/gitfit/android/ui/home/journal/JournalViewModel.kt
+++ app/src/main/java/com/gitfit/android/ui/home/journal/JournalViewModel.kt
@@ -1,13 +1,5 @@
 package com.gitfit.android.ui.home.journal
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class JournalViewModel : ViewModel() {
-
-    private val _text = MutableLiveData<String>().apply {
-        value = "This is journal Fragment"
-    }
-    val text: LiveData<String> = _text
-}
+class JournalViewModel : ViewModel()
